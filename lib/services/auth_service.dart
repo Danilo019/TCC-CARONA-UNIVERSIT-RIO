2f41cac7f8c580b9b871861f3a740f1cec244df2
Danilo019 <danilosantos.silva018@gmail.com>
2025-12-14 23:57:03 -0300

    fix: adiciona backendUrl e httpClient ao EmailService

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
